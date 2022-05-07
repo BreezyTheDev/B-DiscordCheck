3bfa59fbe3bfabeea2bb3421b1c659c0cbf5ed3e
Breezy <63890993+BreezyTheDev@users.noreply.github.com>
2022-05-06 18:00:06 -0700

    B-DiscordCheck

---
+++ client.lua
@@ -0,0 +1,10 @@
+TriggerEvent('chat:addSuggestion', '/discordcheck', 'Checks for targeted players discord information.', {
+    {
+        name = "action",
+        help = "name or nick | name will prompt you their discord name & nick will prompt you their nickname."
+    },
+    {
+        name = "playerID",
+        help = "The player's server ID."
+    }
+})
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+author "Creator: BreezyTheDev | Discord: Breezy#0001"
+description "BreezyTheDev's Discord Check Resource. Version 1.3"
+version "1.3"
+
+fx_version "cerulean"
+game "gta5"
+
+client_scripts {
+	"client.lua"
+}
+
+server_scripts {
+	"server.lua",
+}
+
+dependency 'Badger_Discord_API'
